68a8248b8f530065b06edd6f163ef53f566208ab
code0wizard <akkaouimohamed00@gmail.com>
2023-09-07 17:28:52 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point prints a string without using any listed functions
+ * main - prints a string without using any listed functions
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 
+			2015-10-19\n", stdout);
 	return (1);
 }
