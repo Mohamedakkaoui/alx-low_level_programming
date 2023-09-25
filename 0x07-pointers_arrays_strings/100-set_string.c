836c0712a2c45c25752db79eef0b1cb551654ccf
code0wizard <akkaouimohamed00@gmail.com>
2023-09-25 15:34:36 -0700

    function that sets te value of a pointer to a char

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,8 +4,9 @@
  * set_string - function that sets te value of a pointer to a char
  *
  * @s: pointer to a pointer
- *
  * @to: pointer char
+ *
+ * Return : Always 0 (Success)
  */
 
 void set_string(char **s, char *to)
