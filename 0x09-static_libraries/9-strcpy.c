e744415151ff9f3643e4c0b77929514919e167e5
code0wizard <akkaouimohamed00@gmail.com>
2023-10-02 08:42:35 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string
+ *
+ * @dest: pointer of a character to copy to
+ * @src: pointer of a character to copy from
+ *
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for ( ; j < i ; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
