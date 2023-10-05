8cf628ef424adbfbd0dcee470a0e4e3380f1a534
code0wizard <akkaouimohamed00@gmail.com>
2023-10-04 17:33:05 -0700

    function that returns a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- * free_grid - function that frees a 2 dim grid
+/**
+ *free_grid - function that frees a 2 dim grid
  *
- * @grid : a matrix
- * @heigt : height of grid
+ * @grid: a grid wit 2 dim
+ * @height: height of grid
  *
- * Return : Always 0.
+ * Return : no return
  */
+
 void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
-		for (;height >= 0; height--)
+		for (; height >= 0; height--)
 		{
 			free(grid[height]);
 			free(grid);
