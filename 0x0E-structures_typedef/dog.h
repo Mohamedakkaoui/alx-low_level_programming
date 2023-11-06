e0c56dce79aa239b94e3b2e5d18f6e41f9276956
Mohamedakkaoui <akkaouimohamed00@gmail.com>
2023-11-06 13:03:14 -0800

    struct dog with te following elements name /owner/age

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct to old data of the dog
+ *
+ *@name : name of te dog
+ *@owner : owner of the dog
+ *@age : age of the dog
+ *
+ *this structure is used to reprsent data of the dog
+ */
 struct dog
 {
 	char *name;
