499ba35b9302a40e55e17c07b35eb01d95fdce73
Mohamedakkaoui <akkaouimohamed00@gmail.com>
2023-11-06 16:13:52 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#define struct dog-t
 /**
  * struct dog - struct to old data of the dog
  *
@@ -18,5 +17,5 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+#define struct dog_t
 #endif
