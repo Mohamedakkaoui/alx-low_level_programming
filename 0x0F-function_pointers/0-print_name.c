c02410f9751c3a361790602c6a112186260ae9ab
Mohamedakkaoui <akkaouimohamed00@gmail.com>
2023-11-08 09:45:20 -0800

    deploy

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ *
+ * @name : the name to print
+ * @f: function pointer
+ *
+ *Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
