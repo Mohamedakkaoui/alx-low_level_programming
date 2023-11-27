251820feea532111bdafba7d258335dd6bfcd37e
akkaouimohamed <akkaouimohamed00@gmail.com>
2023-11-27 04:16:56 -0800

    struct dog with te following eements name / age / owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
-	char * name;
+/**
+ * struct dog - Represents a dog
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure represents information about a dog,
+ * including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
 	float age;
 	char *owner;
 };
