474b33238fe494e3bb521d0ac952005a458cda56
akkaouimohamed <akkaouimohamed00@gmail.com>
2023-12-18 16:59:39 -0800

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * append_text_to_file - function that appends text at the end of a file
+ * create_file - function that creates a file
  *@filename : name of the file
  *@text_content : NULL terminated string
  *Return: 1 on success and -1 on failure
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int i, b, a = 0;
 
